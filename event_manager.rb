eb986cb8ce4d1278f669ab142876438dba650c27
Fabricio Carrara <fabricio_x@hotmail.com>
2016-04-05 12:05:07 +0500

    Finish iterations pending

--- event_manager.rb
+++ event_manager.rb
@@ -20,6 +20,19 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
 end
 
+def clean_phone(phone)
+  phone = phone.scan(/[0-9]/).join
+  if phone.length == 11 && phone[0] == "1"
+    phone = phone[1..10]
+  end
+  phone
+end
+
+def valid_phone?(phone)
+  phone = clean_phone(phone)
+  return phone.length == 10 ? true : false
+end
+
 def legislators_by_zipcode(zipcode)
   legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
@@ -33,12 +46,37 @@ def save_thank_you_letters(id, form_letter)
   end
 end
 
+def print_registration_date(registration_dates)
+  hours = []
+  days_week = []
+
+  registration_dates.each do |date| 
+    date = DateTime.strptime(date, "%m/%d/%y %H:%M")
+    hours << date.hour
+    days_week << date.wday
+  end
+
+  # Group every hour
+  hours = hours.group_by { |i| i }
+  puts " Hour  Registrations"
+  hours.each { |index,i| puts "#{index}:00       #{i.count}" }
+
+  puts ""
+
+  # Group day of the week
+  days_week = days_week.group_by { |i| i }
+  puts "Day of the week  Registrations"
+  days_week.each { |index, i| puts "    #{index}                  #{i.count}" }
+  
+end
+
 puts "EventManager initialized."
 
 content = CSV.open("event_attendees.csv", headers: true, header_converters: :symbol)
 
 template_letter = File.read("form_letter.erb")
 erb_template = ERB.new template_letter
+registration_dates = []
 
 content.each do |row|
   id = row[0]
@@ -52,5 +90,16 @@ content.each do |row|
 
   save_thank_you_letters(id, form_letter)
 
+  # Iteration: Clean phone numbers
+  puts valid_phone?(row[:homephone])
+
+  registration_dates << row[:regdate]
+
 end
+
+# Iteration : Time targeting and Iteration: Day of the week targeting
+# Print the list of registrations per hour and day of the week.
+# With this list we can order descendent, select peak hours, etc
+print_registration_date(registration_dates)
+
 
